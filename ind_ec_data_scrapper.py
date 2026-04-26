fbed9f85487e3e95f9f37e5eb5e3953464c94eeb
loopsandlogic <loopsandlogic@gmail.com>
2026-04-26 12:05:30 -0500

    COde updated

--- ind_ec_data_scrapper.py
+++ ind_ec_data_scrapper.py
@@ -19,20 +19,32 @@ def get_state_details(state: str) -> dict:
     """Returns state name, state code, and number of constituencies."""
 
     state_df = pd.read_csv("data/ind-state-constituency-details.csv")
-    state_details = state_df[state_df["state_name"].str.lower() == state.lower()].iloc[0]
+    state_details = state_df[state_df["State"].str.lower() == state.lower()].iloc[0]
     return {
-        "state_name": state_details["state_name"],
-        "state_code": state_details["state_code"],
-        "assembly_seats": state_details["assembly_seats"].item(),
+        "State": state_details["State"],
+        "State_Code": state_details["State_Code"],
+        "Assembly_Seats": state_details["Assembly_Seats"].item(),
     }
 
-def get_coalition_parties() -> pd.DataFrame:
-    return pd.read_csv("data/bihar_coalitions.csv")
+def get_coalition_parties(year: int, state: str) -> pd.DataFrame:
+    state = state.title()
+    df = pd.read_csv("data/ind-state-coalitions.csv")
+    df = df[(df["Year"] == year) & (df["State"] == state)].drop(["State", "Year"], axis=1)
+
+    return df
 
 def get_base_url(state: str, year: int) -> str:
+    state = state.title()
     base_url_df = pd.read_csv("data/ind-state-ec-url.csv")
     base_url = base_url_df[(base_url_df["state"] == state) & (base_url_df["year"] == year)].iloc[0]["base_url"]
     return base_url
+
+def get_state_constituency_details(state: str) -> pd.DataFrame:
+    state = state.title()
+    df = pd.read_csv("data/constituency_details.csv")
+    df = df[["Constituency_Num", "District", "Constituency_Category"]]
+
+    return df
 
 def get_voting_tally(base_url: str, total_constituency: int) -> pd.DataFrame:
     seq_no = 1
@@ -52,7 +64,7 @@ def get_voting_tally(base_url: str, total_constituency: int) -> pd.DataFrame:
     driver = webdriver.Chrome(options=options)
 
     while seq_no <= total_constituency:
-        print(f"Scraping data for constituency number {seq_no}...")
+        print(f"Scraping data for constituency number {seq_no} ...")
         driver.get(source_url(base_url, seq_no))
         WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.TAG_NAME, "tbody")))
 
@@ -61,27 +73,46 @@ def get_voting_tally(base_url: str, total_constituency: int) -> pd.DataFrame:
         candidates = driver.find_element(By.TAG_NAME, "tbody")
 
         state = h2.split("(")[1].replace(")", "").strip()
-        constituency = h2.split("(")[0].split("-")[1].strip()
-        election_year = h1.split("-")[-1].strip()
-        election_type = h2.split()[0]
+        Constituency_Name = h2.split("(")[0].split("-")[1].strip()
 
+        elem = driver.find_element(By.CSS_SELECTOR, "div.round-status")
+        total_round = elem.text.split("/")[-1]
+        curr_round = elem.text.split("/")[0].split()[-1]
 
         fieldnames = ["serial_num", "candidate_name", "party", "evmvotes", "postalvotes", "totalvotes", "vote_percent"]
 
         for candidate in candidates.find_elements(By.TAG_NAME, "tr"):
-            results["constituency_no"] = seq_no
-            results["constituency"] = constituency
+            results["Constituency_Num"] = seq_no
+            results["Constituency_Name"] = Constituency_Name
+            
             voting_tally = dict(zip(fieldnames, map(lambda d: d.text, candidate.find_elements(By.TAG_NAME, "td"))))
             results = results | voting_tally
+
+            results["Total_Rounds"] = total_round
+            results["Current_Round"] = curr_round
+            results["State"] = state
+
             results_df = pd.DataFrame([results])
             df = pd.concat([df, results_df], ignore_index=True)
 
         seq_no += 1
     
-    df["constituency"] = df["constituency"].str.title()
+    df["Constituency_Name"] = df["Constituency_Name"].str.title()
     df["candidate_name"] = df["candidate_name"].str.title()
+    df = df.rename(columns={"candidate_name": "Candidate_Name",
+                            "party": "Party_Name",
+                            "evmvotes": "EVM_Votes",
+                            "postalvotes": "Postal_Votes",
+                            "totalvotes": "Total_Votes",
+                            "vote_percent": "Vote_Percent",
+                            "serial_num": "Serial_Num"})
 
     driver.quit()
+
+    cd = get_state_constituency_details(state)
+
+    df = df.merge(cd, left_on="Constituency_Num", right_on="Constituency_Num", how="left")
+
     return df
 
 def load_to_databricks(remote_file: str, local_file: str):
@@ -103,12 +134,14 @@ def load_to_databricks(remote_file: str, local_file: str):
     print(response.status_code, response.text)
 
 def get_state_result(state: str, voting_year: int) -> pd.DataFrame:
-    total_constituencies = get_state_details(state)["assembly_seats"]
+    total_constituencies = get_state_details(state)["Assembly_Seats"]
     url = get_base_url(state, voting_year)
 
     df = get_voting_tally(url, total_constituencies)
-    coal_df = get_coalition_parties()
-    return df.join(coal_df.set_index("party"), on="party", how="left")
+    coal_df = get_coalition_parties(voting_year, state)
+    df = df.merge(coal_df, left_on="Party_Name", right_on="Party_Name", how="left")
+    df["Election_Year"] = voting_year
+    return df
 
 def load_results_to_db(state: str, voting_year: int):
 
@@ -117,15 +150,15 @@ def load_results_to_db(state: str, voting_year: int):
     
     df = get_state_result(state, voting_year)
 
-    df.to_csv(f"data/{state.lower()}_election_results_{start_time}.csv", index=False, header=True)
+    df.to_csv(f"data/{state.lower()}_election_results_{voting_year}.csv", index=False, header=True)
     
     end_time = datetime.now().strftime("%Y%m%d%H%M")
     print(f"{end_time}: {state} Election Data Scrapper completed successfully. Data saved to '{state.lower()}_election_results_{start_time}.csv'.") 
 
 
     load_to_databricks(
-        remote_file=f"{state.lower()}_election_results_{start_time}.csv",
-        local_file=f"data/{state.lower()}_election_results_{start_time}.csv"
+        remote_file=f"{state.lower()}_election_results_{voting_year}.csv",
+        local_file=f"data/{state.lower()}_election_results_{voting_year}.csv"
     )
 
 
